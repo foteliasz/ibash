285d7bdabbf7eadc3824efa1c3113f68c84a5adf
foteliasz <58996128+foteliasz@users.noreply.github.com>
2022-11-22 20:34:33 +0100

    Switched to Avalonia

---
+++ src/ibash/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Linq;
+using Avalonia.Media;
+
+namespace ibash.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    public string Debug { get; private set; }
+    
+    public string Command => "Welcome to Avalonia!";
+
+    public MainWindowViewModel()
+    {
+    }
+}
